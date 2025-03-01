74e2dca52ad553895f2e559cf609703a16524d72
SaraEOlivera <saraeolivera@gmail.com>
2025-02-28 21:19:16 -0300

    Se agregan eventos para limpiar datos y el ingreso de datos

---
+++ formPrincipal/frmInscripcionPostulante.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace formPrincipal
+{
+    partial class frmInscripcionPostulante
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboFotoInscripcion = new System.Windows.Forms.PictureBox();
+            this.lblTituloInscripcion = new System.Windows.Forms.Label();
+            this.lblNombreInscripcion = new System.Windows.Forms.Label();
+            this.txtNombreInscripcion = new System.Windows.Forms.TextBox();
+            this.txtApellidoInscripcion = new System.Windows.Forms.TextBox();
+            this.lblApellidoInscripcion = new System.Windows.Forms.Label();
+            this.lblTipoInscripcion = new System.Windows.Forms.Label();
+            this.cbxTipo = new System.Windows.Forms.ComboBox();
+            this.txtDocumentoInscripcion = new System.Windows.Forms.TextBox();
+            this.lblDocumentoInscripcion = new System.Windows.Forms.Label();
+            this.btnIngresarInscripcion = new System.Windows.Forms.Button();
+            this.btnLimpiarInscripcion = new System.Windows.Forms.Button();
+            this.btnVolverInscripcion = new System.Windows.Forms.Button();
+            this.lblAstersico = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cboFotoInscripcion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboFotoInscripcion
+            // 
+            this.cboFotoInscripcion.Location = new System.Drawing.Point(31, 65);
+            this.cboFotoInscripcion.Name = "cboFotoInscripcion";
+            this.cboFotoInscripcion.Size = new System.Drawing.Size(207, 198);
+            this.cboFotoInscripcion.TabIndex = 0;
+            this.cboFotoInscripcion.TabStop = false;
+            // 
+            // lblTituloInscripcion
+            // 
+            this.lblTituloInscripcion.AutoSize = true;
+            this.lblTituloInscripcion.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloInscripcion.Location = new System.Drawing.Point(229, 23);
+            this.lblTituloInscripcion.Name = "lblTituloInscripcion";
+            this.lblTituloInscripcion.Size = new System.Drawing.Size(220, 19);
+            this.lblTituloInscripcion.TabIndex = 1;
+            this.lblTituloInscripcion.Text = "Inscripción del Postulante";
+            // 
+            // lblNombreInscripcion
+            // 
+            this.lblNombreInscripcion.AutoSize = true;
+            this.lblNombreInscripcion.Location = new System.Drawing.Point(270, 65);
+            this.lblNombreInscripcion.Name = "lblNombreInscripcion";
+            this.lblNombreInscripcion.Size = new System.Drawing.Size(44, 13);
+            this.lblNombreInscripcion.TabIndex = 2;
+            this.lblNombreInscripcion.Text = "Nombre";
+            // 
+            // txtNombreInscripcion
+            // 
+            this.txtNombreInscripcion.Location = new System.Drawing.Point(327, 65);
+            this.txtNombreInscripcion.Name = "txtNombreInscripcion";
+            this.txtNombreInscripcion.Size = new System.Drawing.Size(240, 20);
+            this.txtNombreInscripcion.TabIndex = 3;
+            // 
+            // txtApellidoInscripcion
+            // 
+            this.txtApellidoInscripcion.Location = new System.Drawing.Point(327, 116);
+            this.txtApellidoInscripcion.Name = "txtApellidoInscripcion";
+            this.txtApellidoInscripcion.Size = new System.Drawing.Size(240, 20);
+            this.txtApellidoInscripcion.TabIndex = 5;
+            // 
+            // lblApellidoInscripcion
+            // 
+            this.lblApellidoInscripcion.AutoSize = true;
+            this.lblApellidoInscripcion.Location = new System.Drawing.Point(270, 116);
+            this.lblApellidoInscripcion.Name = "lblApellidoInscripcion";
+            this.lblApellidoInscripcion.Size = new System.Drawing.Size(44, 13);
+            this.lblApellidoInscripcion.TabIndex = 4;
+            this.lblApellidoInscripcion.Text = "Apellido";
+            // 
+            // lblTipoInscripcion
+            // 
+            this.lblTipoInscripcion.AutoSize = true;
+            this.lblTipoInscripcion.Location = new System.Drawing.Point(273, 168);
+            this.lblTipoInscripcion.Name = "lblTipoInscripcion";
+            this.lblTipoInscripcion.Size = new System.Drawing.Size(28, 13);
+            this.lblTipoInscripcion.TabIndex = 6;
+            this.lblTipoInscripcion.Text = "Tipo";
+            // 
+            // cbxTipo
+            // 
+            this.cbxTipo.FormattingEnabled = true;
+            this.cbxTipo.Location = new System.Drawing.Point(307, 168);
+            this.cbxTipo.Name = "cbxTipo";
+            this.cbxTipo.Size = new System.Drawing.Size(68, 21);
+            this.cbxTipo.TabIndex = 7;
+            // 
+            // txtDocumentoInscripcion
+            // 
+            this.txtDocumentoInscripcion.Location = new System.Drawing.Point(477, 168);
+            this.txtDocumentoInscripcion.Name = "txtDocumentoInscripcion";
+            this.txtDocumentoInscripcion.Size = new System.Drawing.Size(90, 20);
+            this.txtDocumentoInscripcion.TabIndex = 9;
+            // 
+            // lblDocumentoInscripcion
+            // 
+            this.lblDocumentoInscripcion.AutoSize = true;
+            this.lblDocumentoInscripcion.Location = new System.Drawing.Point(409, 171);
+            this.lblDocumentoInscripcion.Name = "lblDocumentoInscripcion";
+            this.lblDocumentoInscripcion.Size = new System.Drawing.Size(62, 13);
+            this.lblDocumentoInscripcion.TabIndex = 8;
+            this.lblDocumentoInscripcion.Text = "Documento";
+            // 
+            // btnIngresarInscripcion
+            // 
+            this.btnIngresarInscripcion.Location = new System.Drawing.Point(276, 240);
+            this.btnIngresarInscripcion.Name = "btnIngresarInscripcion";
+            this.btnIngresarInscripcion.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresarInscripcion.TabIndex = 10;
+            this.btnIngresarInscripcion.Text = "Ingresar";
+            this.btnIngresarInscripcion.UseVisualStyleBackColor = true;
+            this.btnIngresarInscripcion.Click += new System.EventHandler(this.btnIngresarInscripcion_Click);
+            // 
+            // btnLimpiarInscripcion
+            // 
+            this.btnLimpiarInscripcion.Location = new System.Drawing.Point(386, 240);
+            this.btnLimpiarInscripcion.Name = "btnLimpiarInscripcion";
+            this.btnLimpiarInscripcion.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiarInscripcion.TabIndex = 11;
+            this.btnLimpiarInscripcion.Text = "Limpiar";
+            this.btnLimpiarInscripcion.UseVisualStyleBackColor = true;
+            // 
+            // btnVolverInscripcion
+            // 
+            this.btnVolverInscripcion.Location = new System.Drawing.Point(492, 240);
+            this.btnVolverInscripcion.Name = "btnVolverInscripcion";
+            this.btnVolverInscripcion.Size = new System.Drawing.Size(75, 23);
+            this.btnVolverInscripcion.TabIndex = 12;
+            this.btnVolverInscripcion.Text = "Volver";
+            this.btnVolverInscripcion.UseVisualStyleBackColor = true;
+            this.btnVolverInscripcion.Click += new System.EventHandler(this.btnVolverInscripcion_Click);
+            // 
+            // lblAstersico
+            // 
+            this.lblAstersico.AutoSize = true;
+            this.lblAstersico.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAstersico.ForeColor = System.Drawing.Color.Red;
+            this.lblAstersico.Location = new System.Drawing.Point(573, 65);
+            this.lblAstersico.Name = "lblAstersico";
+            this.lblAstersico.Size = new System.Drawing.Size(17, 24);
+            this.lblAstersico.TabIndex = 13;
+            this.lblAstersico.Text = "*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(573, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(573, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 24);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(381, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "*";
+            // 
+            // frmInscripcionPostulante
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 286);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAstersico);
+            this.Controls.Add(this.btnVolverInscripcion);
+            this.Controls.Add(this.btnLimpiarInscripcion);
+            this.Controls.Add(this.btnIngresarInscripcion);
+            this.Controls.Add(this.txtDocumentoInscripcion);
+            this.Controls.Add(this.lblDocumentoInscripcion);
+            this.Controls.Add(this.cbxTipo);
+            this.Controls.Add(this.lblTipoInscripcion);
+            this.Controls.Add(this.txtApellidoInscripcion);
+            this.Controls.Add(this.lblApellidoInscripcion);
+            this.Controls.Add(this.txtNombreInscripcion);
+            this.Controls.Add(this.lblNombreInscripcion);
+            this.Controls.Add(this.lblTituloInscripcion);
+            this.Controls.Add(this.cboFotoInscripcion);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(646, 325);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(646, 325);
+            this.Name = "frmInscripcionPostulante";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inscribir Postulante";
+            this.Load += new System.EventHandler(this.InscribirPostulante_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cboFotoInscripcion)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cboFotoInscripcion;
+        private System.Windows.Forms.Label lblTituloInscripcion;
+        private System.Windows.Forms.Label lblNombreInscripcion;
+        private System.Windows.Forms.TextBox txtNombreInscripcion;
+        private System.Windows.Forms.TextBox txtApellidoInscripcion;
+        private System.Windows.Forms.Label lblApellidoInscripcion;
+        private System.Windows.Forms.Label lblTipoInscripcion;
+        private System.Windows.Forms.ComboBox cbxTipo;
+        private System.Windows.Forms.TextBox txtDocumentoInscripcion;
+        private System.Windows.Forms.Label lblDocumentoInscripcion;
+        private System.Windows.Forms.Button btnIngresarInscripcion;
+        private System.Windows.Forms.Button btnLimpiarInscripcion;
+        private System.Windows.Forms.Button btnVolverInscripcion;
+        private System.Windows.Forms.Label lblAstersico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ formPrincipal/frmInscripcionPostulante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formPrincipal
+{
+    public partial class frmInscripcionPostulante : Form
+    {
+        public frmInscripcionPostulante()
+        {
+            InitializeComponent();
+        }
+
+        private void InscribirPostulante_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolverInscripcion_Click(object sender, EventArgs e)
+        {
+            formPrincipal principal = new formPrincipal();
+            principal.Show();
+            this.Hide();
+        }
+
+        private void btnIngresarInscripcion_Click(object sender, EventArgs e)
+        {
+            if
+        }
+    }
+}
